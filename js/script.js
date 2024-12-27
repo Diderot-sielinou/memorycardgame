737c76d1d72e24b083ef9c70f44492f88eaa5bc7
diderot-sielinou <diderotsielinou@gmail.com>
2024-12-27 03:25:03 +0100

    memory finish

--- js/script.js
+++ js/script.js
@@ -17,24 +17,46 @@ const emojieTab = [
   "🧑‍💻",
 ];
 // console.log(emojieTab);
-let buton = document.querySelector(".restart")
-buton.addEventListener("click",()=>window.location.reload())
-let newTab = emojieTab.sort(()=> (Math.random()> 0.5)?2:-1);
-for(let item of newTab ){
+let buton = document.querySelector(".restart");
+buton.addEventListener("click", () => window.location.reload());
+let newTab = emojieTab.sort(() => (Math.random() > 0.5 ? 2 : -1));
+for (let item of newTab) {
   let box = document.createElement("div");
   box.className = "item";
   box.innerHTML = item;
-  box.onclick = ()=>{
+  box.onclick = () => {
     box.classList.add("boxopen");
-  }
-  document.querySelector(".game").append(box)
+    setTimeout(() => {
+      if (document.querySelectorAll(".boxopen").length > 1) {
+        if (
+          (document.querySelectorAll(".boxopen")[0].innerHTML ==
+            document.querySelectorAll(".boxopen")[1].innerHTML)
+        ) {
+          document.querySelectorAll(".boxopen")[0].classList.add("boxmatch");
+          document.querySelectorAll(".boxopen")[1].classList.add("boxmatch");
+
+          document.querySelectorAll(".boxopen")[0].classList.remove("boxopen");
+          // document.querySelectorAll(".boxopen")[1].classList.remove("boxopen");
+
+          if (
+            document.querySelectorAll(".boxmatch").length == emojieTab.length
+          ) {
+            alert("you win 👍👍👍👍");
+          }
+        } else {
+          document.querySelectorAll(".boxopen")[1].classList.remove("boxopen");
+          document.querySelectorAll(".boxopen")[0].classList.remove("boxopen");
+        }
+      }
+    }, 500);
+  };
+  document.querySelector(".game").append(box);
 }
 
 // document.querySelectorAll(".item").forEach((Item)=>{
 //   Item.addEventListener("click",()=>{
 //     Item.classList.add("boxopen")
 //     console.log("ad")
-
 
 //   })
 // })
