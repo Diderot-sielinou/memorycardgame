e3f8401eb52a25eea1630a3d48fbe06e940f8245
diderot-sielinou <diderotsielinou@gmail.com>
2025-01-05 01:14:22 +0100

    add linter standar

--- js/script.js
+++ js/script.js
@@ -1,99 +1,99 @@
+/* global alert */
 const emojieTab = [
-  "🇨🇦",
-  "🇨🇦",
-  "🇦🇲",
-  "🇦🇲",
-  "🍺",
-  "🍺",
-  "👍",
-  "👍",
-  "💔",
-  "💔",
-  "😖",
-  "😖",
-  "😍",
-  "😍",
-  "🧑‍💻",
-  "🧑‍💻",
-];
+  '🇨🇦',
+  '🇨🇦',
+  '🇦🇲',
+  '🇦🇲',
+  '🍺',
+  '🍺',
+  '👍',
+  '👍',
+  '💔',
+  '💔',
+  '😖',
+  '😖',
+  '😍',
+  '😍',
+  '🧑‍💻',
+  '🧑‍💻'
+]
 // console.log(emojieTab);
-let buton = document.querySelector(".restart");
-buton.addEventListener("click", () => window.location.reload());
-let newTab = emojieTab.sort(() => (Math.random() > 0.5 ? 2 : -1));
-for (let item of newTab) {
-  let box = document.createElement("div");
-  box.className = "item";
-  box.innerHTML = item;
+const buton = document.querySelector('.restart')
+buton.addEventListener('click', () => window.location.reload())
+const newTab = emojieTab.sort(() => (Math.random() > 0.5 ? 2 : -1))
+for (const item of newTab) {
+  const box = document.createElement('div')
+  box.className = 'item'
+  box.innerHTML = item
   box.onclick = () => {
-    box.classList.add("boxopen");
+    box.classList.add('boxopen')
     setTimeout(() => {
-      if (document.querySelectorAll(".boxopen").length > 1) {
+      if (document.querySelectorAll('.boxopen').length > 1) {
         if (
-          document.querySelectorAll(".boxopen")[0].innerHTML ==
-          document.querySelectorAll(".boxopen")[1].innerHTML
+          document.querySelectorAll('.boxopen')[0].innerHTML ===
+          document.querySelectorAll('.boxopen')[1].innerHTML
         ) {
-          document.querySelectorAll(".boxopen")[0].classList.add("boxmatch");
-          document.querySelectorAll(".boxopen")[1].classList.add("boxmatch");
+          document.querySelectorAll('.boxopen')[0].classList.add('boxmatch')
+          document.querySelectorAll('.boxopen')[1].classList.add('boxmatch')
 
-          document.querySelectorAll(".boxopen")[0].classList.remove("boxopen");
+          document.querySelectorAll('.boxopen')[0].classList.remove('boxopen')
           // document.querySelectorAll(".boxopen")[1].classList.remove("boxopen");
 
           if (
-            document.querySelectorAll(".boxmatch").length == emojieTab.length
+            document.querySelectorAll('.boxmatch').length === emojieTab.length
           ) {
-            alert("you win 👍👍👍👍");
+            alert('you win 👍👍👍👍')
           }
         } else {
-          document.querySelectorAll(".boxopen")[1].classList.remove("boxopen");
-          document.querySelectorAll(".boxopen")[0].classList.remove("boxopen");
+          document.querySelectorAll('.boxopen')[1].classList.remove('boxopen')
+          document.querySelectorAll('.boxopen')[0].classList.remove('boxopen')
         }
       }
-    }, 500);
-  };
-  document.querySelector(".game").append(box);
+    }, 500)
+  }
+  document.querySelector('.game').append(box)
 }
 
-let timer;
-let totalSeconds = 60;
-function startCountdown() {
+let timer
+let totalSeconds = 60
+function startCountdown () {
   timer = setInterval(() => {
     if (totalSeconds <= 0) {
-      clearInterval(timer);
-      document.querySelector(".chromometre").textContent = "Temps écoulé !";
-      changeAfterTimeOver();
-      return;
+      clearInterval(timer)
+      document.querySelector('.chromometre').textContent = 'Temps écoulé !'
+      changeAfterTimeOver()
+      return
     }
 
-    totalSeconds--;
+    totalSeconds--
 
-    const minutes = Math.floor(totalSeconds / 60);
-    const seconds = totalSeconds % 60;
+    const minutes = Math.floor(totalSeconds / 60)
+    const seconds = totalSeconds % 60
 
-    document.querySelector(".chromometre").textContent =
-      String(minutes).padStart(2, "0") + ":" + String(seconds).padStart(2, "0");
-  }, 1000);
+    document.querySelector('.chromometre').textContent =
+      String(minutes).padStart(2, '0') + ':' + String(seconds).padStart(2, '0')
+  }, 1000)
 }
 
 // fonction pour cache les sections
-let isclik = false;
-function changeSection() {
-  let section1 = document.querySelector(".started");
-  let section2 = document.querySelector(".container");
-  isclik = !isclik;
-  console.log(isclik);
+let isclik = false
+function changeSection () {
+  const section1 = document.querySelector('.started')
+  const section2 = document.querySelector('.container')
+  isclik = !isclik
+  console.log(isclik)
   if (isclik) {
-    section1.style.display = "none";
-    section2.style.display = "flex";
-    startCountdown();
+    section1.style.display = 'none'
+    section2.style.display = 'flex'
+    startCountdown()
   }
 }
 
-function changeAfterTimeOver(){
-  let section1 = document.querySelector(".started");
-  let section2 = document.querySelector(".container");
-  section1.style.display = "flex";
-   section2.style.display = "none";
+function changeAfterTimeOver () {
+  const section1 = document.querySelector('.started')
+  const section2 = document.querySelector('.container')
+  section1.style.display = 'flex'
+  section2.style.display = 'none'
 }
 
-
-document.querySelector(".start-btn").addEventListener("click",changeSection);
+document.querySelector('.start-btn').addEventListener('click', changeSection)
